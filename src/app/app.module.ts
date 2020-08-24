0c3f7cb9c0c845a1c170b7d208a6802ae85ff763
Eric C <39015447+ricetech@users.noreply.github.com>
2020-08-24 18:47:44 -0400

    Create static-pages.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { StaticPagesModule } from './static-pages/static-pages.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     CoreModule,
+    StaticPagesModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
