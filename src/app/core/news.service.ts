2ba6a706a7b3579d354c640e5ca7a29999012fb5
Akanksha Monga <akanksha.monga17@gmail.com>
2020-08-29 23:17:40 -0700

    Create getArticles service

--- src/app/core/news.service.ts
+++ src/app/core/news.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { NewsArticle } from '../shared/interfaces';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
+  private handleError(error: any): Observable<never> {
+      console.error('server error:', error);
+      if (error.error instanceof Error) {
+        const errMessage = error.error.message;
+        return Observable.throw(errMessage);
+      }
+      return Observable.throw(error || 'Server error');
+  }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getArticles(): Observable<NewsArticle[]> {
+    return this.http.get<NewsArticle[]>('assets/dummy-article-data.json')
+      .pipe(catchError(this.handleError)
+      );
+
+  }
 }
