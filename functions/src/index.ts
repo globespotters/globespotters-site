c71b206ba7761c789f1b03627183a7d9f1d9278d
Jonathan Papineau <jonapap2001@gmail.com>
2020-08-30 13:20:51 -0400

    Remove unnecessary tests from index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,4 @@
 //import * as functions from 'firebase-functions';
-import { getLocationFromArticle, getLocationsFromArticleList, GroupedLocationGetter } from './wikiLocation';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -8,25 +7,3 @@ import { getLocationFromArticle, getLocationsFromArticleList, GroupedLocationGet
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-
-getLocationFromArticle("Ottawa").then((coordinates) => {
-  console.log(coordinates);
-});
-
-getLocationsFromArticleList(["Ottawa", "Toronto", "Hearst, Ontario", "Poutine"]).then((coordinates) => {
-  console.log(coordinates);
-});
-
-// Next lines try to simulate a bit what will happen when we will request asynchronously a bunch of locations.
-const locationGetter = new GroupedLocationGetter()
-const articleList = ["Ottawa", "Toronto", "Hearst, Ontario", "Poutine", "Ottawa", "Dubai", "Canada"];
-
-for (const article of articleList) {
-  (async (a) => {
-    const coordinates = await locationGetter.getLocationFromArticle(a);
-
-    console.log(a, coordinates);
-  })(article)
-}
-
-locationGetter.flushRequests();
