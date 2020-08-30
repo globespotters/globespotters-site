7cad0f6addfcc1b521505a6e3c8e4e5d71de7491
Akanksha Monga <akanksha.monga17@gmail.com>
2020-08-30 00:24:18 -0700

    Implement getArticles in timeline component

--- src/app/map/timeline/timeline.component.ts
+++ src/app/map/timeline/timeline.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {TimelineService} from "../../core/timeline.service";
+import { TimelineService } from '../../core/timeline.service';
+import { NewsService } from '../../core/news.service';
+import { NewsArticle } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.scss']
+  styleUrls: [ './timeline.component.scss' ]
 })
 export class TimelineComponent implements OnInit {
+  public articles: NewsArticle[] = [];
 
-  constructor(public timelineService: TimelineService) { }
+  constructor(public timelineService: TimelineService, private newsService: NewsService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.newsService.getArticles().subscribe(
+      (articles: NewsArticle[]) => {
+        this.articles = articles;
+      },
+      error => {
+        console.log('Uh-oh' + error);
+      }
+    );
+  }
 
 }
