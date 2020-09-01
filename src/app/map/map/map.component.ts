60cc030a7bd00da0279ba2dc4f899b944316d7d7
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-09-01 08:49:09 -0400

    Prevent map from possibly being undefined

--- src/app/map/map/map.component.ts
+++ src/app/map/map/map.component.ts
@@ -11,7 +11,7 @@ export const initial_LngLat = new mapboxgl.LngLat(-123.9749, 40.7736);
   styleUrls: [ './map.component.scss' ]
 })
 export class MapComponent implements OnInit {
-  public map: mapboxgl.Map | undefined;
+  public map!: mapboxgl.Map | undefined;
   public STYLE_LIGHT: string = 'mapbox://styles/guyinfridge/ckee54njf0kqu19mna1jgz8z1';
   public STYLE_DARK: string = 'mapbox://styles/guyinfridge/ckedd8l2n1n4c19mlyapazlqu';
 
